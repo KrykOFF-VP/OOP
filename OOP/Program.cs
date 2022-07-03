f4352104b087bed8ac830b6404d67097969ef33d
KrykOFF-VP <krykoff.vp@mail.ru>
2022-07-03 23:34:50 +0300

    Первое задание

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,10 +1,10 @@
 ﻿using OOP;
 
 account account1 = new account();
-account1.number = 12313213;
-account1.balance = 500.12m;
-account1.type = 5;
+account1.Setnumber(12313213);
+account1.Setbalance(500.12m);
+account1.Settype(5);
 
-Console.WriteLine($"Cчет №:{account1.number} типа:{account1.type}, имеет баланс: {account1.balance}");
+Console.WriteLine($"Cчет №:{account1.Getnumber()} типа:{account1.Gettype()}, имеет баланс: {account1.Getbalance()}");
 Console.WriteLine("Программа завершена");
 Console.ReadKey();
--- OOP/account.cs
+++ OOP/account.cs
@@ -2,15 +2,36 @@
 {
     public class account
     {
+        static int _X;
         private int _number;
         private decimal _balance;
         private byte _type;
 
-        public int number {get => _number; set => _number = value;}
-            
-        public decimal balance {get => _balance; set => _balance = value;}
+        public int Getnumber()
+        {
+            return _number;
+        }
+        public void Setnumber(int number)
+        {
+            _number = number;
+        }
+        public decimal Getbalance()
+        {
+            return _balance;
+        }
+        public void Setbalance(decimal balance)
+        {
+            _balance =balance ;
+        }
+        public byte Gettype()
+        {
+            return _type;
+        }
+        public void Settype(byte type)
+        {
+            _type=type;
+        }   
         
-        public byte type { get => _type; set => _type = value; }
         
         
     }
