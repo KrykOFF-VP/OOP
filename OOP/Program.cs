d0227a571bf3ec67a08e8aa3c607cd9f330886ee
KrykOFF-VP <krykoff.vp@mail.ru>
2022-07-03 22:31:22 +0300

    Создание класса с закрытмми полями и возможностью чтения и заполнения

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP;
+
+account account1 = new account();
+account1.number = 12313213;
+account1.balance = 500.12m;
+account1.type = 5;
+
+Console.WriteLine($"Cчет №:{account1.number} типа:{account1.type}, имеет баланс: {account1.balance}");
+Console.WriteLine("Программа завершена");
+Console.ReadKey();
---
+++ OOP/account.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP
+{
+    public class account
+    {
+        private int _number;
+        private decimal _balance;
+        private byte _type;
+
+        public int number {get => _number; set => _number = value;}
+            
+        public decimal balance {get => _balance; set => _balance = value;}
+        
+        public byte type { get => _type; set => _type = value; }
+        
+        
+    }
+}
