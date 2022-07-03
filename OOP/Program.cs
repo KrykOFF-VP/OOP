56926dc2d6fa588a660dbb1ca5d23b05ccb141dc
KrykOFF-VP <krykoff.vp@mail.ru>
2022-07-04 02:24:04 +0300

    Замена всех методов  заполнения , свойствами

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,6 +1,14 @@
 ﻿using OOP;
 
-account account1 = new account(12345.32m,25);
-account account2 = new account(13.88m,35);
+account account1 = new account();
+account1.balance = 15987.58m;
+account1.type = 45;
+
+var account2 = new account();
+account2.balance = 4245.42m;
+account2.type = 1;
+
+Console.WriteLine($"Cчет №:{account1.number} типа:{account1.type}, имеет баланс: {account1.balance}");
+Console.WriteLine($"Cчет №:{account2.number} типа:{account2.type}, имеет баланс: {account2.balance}");
 Console.WriteLine("Программа завершена");
 Console.ReadKey();
--- OOP/account.cs
+++ OOP/account.cs
@@ -4,24 +4,17 @@
     public class account
     {
         private static int _startnumber = 11211321;
-        private decimal _balance;
-        private byte _type;
-
+       
         public int Getnumber()
         {
             int number = _startnumber++;
             return number;
         }
+        public int number { get => Getnumber(); }
 
-        public account(decimal balance, byte type)
-        {
+        public decimal balance { get ; set ; }
 
-            _balance = balance;
-            _type = type;
-            Console.WriteLine($"Cчет №:{Getnumber()} типа: {type} имеет баланс ={balance}");
-
-        }
-
+        public byte type { get; set ; }
 
     }
 }
