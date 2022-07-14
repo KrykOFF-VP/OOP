8be6602d24048790db3d923174ff2da5fbf00bf6
KrykOFF-VP <krykoff.vp@mail.ru>
2022-07-14 23:37:11 +0300

    Создал класс

---
+++ OOP/Building.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace OOP
+{
+    public class Building
+    {
+        private int _Number;
+        public int Number { get => _Number; }
+
+        private int _Height;
+        public int Height { get => _Height; }
+
+        private int _Floors;
+        public int Floors { get => _Floors;}
+
+        private int _Flat;
+        public int Flat { get => _Flat; }
+
+        private int _Entrance; 
+        public int Entrance { get => _Entrance;}
+
+
+
+
+
+    }
+}
