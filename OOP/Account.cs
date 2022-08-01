9c7aff99d7e20c04c41d6dd8e6c5fd5541d085d5
KrykOFF-VP <krykoff.vp@mail.ru>
2022-08-01 18:52:42 +0300

    Выполнение 6ого задания

--- OOP/Account.cs
+++ OOP/Account.cs
@@ -5,19 +5,19 @@
     {
 
 
-        public string Number { get; }
-        public decimal Balance { get; set; }
-        public byte Type { get; set; }
+        public string _Number { get; }
+        public decimal _Balance { get; set; }
+        public byte _Type { get; set; }
 
-        private static int _accountNumber = 114342342;
+        private static int _AccountNumber = 114342342;
 
         public Account(decimal balance, byte type)
         {
-            Number = _accountNumber.ToString();
-            _accountNumber++;
+            _Number = _AccountNumber.ToString();
+            _AccountNumber++;
 
-            Balance = balance;
-            Type = type;
+            _Balance = balance;
+            _Type = type;
         }
 
         /// <summary>
@@ -29,10 +29,10 @@
         public bool TransferMoney(ref Account Akk, decimal money)
         {
 
-            if (Akk.Balance >= money)
+            if (Akk._Balance >= money)
             {
-                this.Balance += money;
-                Akk.Balance -= money;
+                this._Balance += money;
+                Akk._Balance -= money;
                 return true;
 
             }
@@ -41,5 +41,58 @@
 
         }
 
+        public static bool operator ==(Account a, Account b)
+        {
+
+
+            if (a._Balance == b._Balance && a._Type == b._Type)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+
+        public static bool operator !=(Account a, Account b)
+        {
+            if (a._Balance != b._Balance && a._Type != b._Type)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() == typeof(Account))
+            {
+                Account Akk = (Account)obj;
+                if (Akk._Balance == this._Balance && Akk._Type == this._Type)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode() => (this._Number, this._Balance, this._Type).GetHashCode();
+        public override string ToString() => $"Номер счета: {_Number}\nБаланс: {_Balance}\nТип счета: {_Type}";
+
+       
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+Account Akk1 = new Account(15987.58m, 45);
+
+var Akk2 = new Account(4245.42m, 1);
+
+
+Console.WriteLine(Akk1 == Akk2);
+Console.WriteLine(Akk1.GetHashCode());
+Console.WriteLine(Akk2.GetHashCode());
+Console.WriteLine(Akk1.ToString());
+Console.WriteLine(Akk2.ToString());
+
